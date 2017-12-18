b7df791408be1c3b587441fbad584268932cf615
Malviina Hallamaa <malviina.hallamaa@gmail.com>
2017-12-18 22:57:58 +0200

    Update demo.js

--- js/demo.js
+++ js/demo.js
@@ -1,4 +1,4 @@
-// TRIGGERS
+// WAYPOINTS
 
 var trigger0_fade = new Waypoint({
     element: document.getElementById("trigger0"),
@@ -30,37 +30,35 @@ window.addEventListener("scroll", drawTriangle);
 
 function drawTriangle() {
 
-    var section_height = document.getElementById("triangle-section").scrollHeight;
-    var client_height = document.documentElement.clientHeight;
+        var helper = document.getElementById("triangle-helper");
+        var helper_height = helper.scrollHeight;
+        var screen_height = document.documentElement.clientHeight;
+
+        var rect = helper.getBoundingClientRect(); // This is to find the position of the helper in relation to the window screen
+        var helper_scrolled = screen_height - rect.top - 500; // the last value gives some offset so that the drawing begins a bit later then when the sections top comes to view
+
+        if (helper_scrolled < 0) {
+            helper_scrolled = 0;
+        }
+
+        if (helper_scrolled > helper_height) {
+            helper_scrolled = helper_height;
+        }
+
+        var scrollpercent = helper_scrolled / helper_height;
+                    
     
-    var elmnt = document.getElementById("triangle-section");
-    var rect = elmnt.getBoundingClientRect();
-    var y = document.documentElement.clientHeight - rect.top - 500; // the last value gives a little offset so that the drawing starts a bit later then when the sections top comes to view
     
-    if (y < 0) {
-        y = 0;
-    }
-    
-    if (y > section_height) {
-        y = section_height;
-    }
-    
-    var scrollpercent = y / document.getElementById("triangle-section").scrollHeight;
-    
-    
-    document.getElementById("timer").innerHTML = y + "<br>" + section_height + "<br>" + client_height + "<br>" + scrollpercent;
+    document.getElementById("timer").innerHTML = helper_scrolled + "<br>" + helper_height + "<br>" + screen_height + "<br>" + scrollpercent;
 
     var draw = length_triangle * scrollpercent;
 
     triangle.style.strokeDashoffset = length_triangle - draw;
 };
 
-
 // DRAWING THE ROUTE ON THE MAP
 
 var reitti = document.getElementById("reitti");
-
-// The length of the polyline needed some tweaking as the getTotalLength didn't work on Firefox. But this fixes it.
 
 var length_reitti = 0;
 var prevPos;
@@ -99,11 +97,14 @@ function drawReitti() {
     
     var scrollpercent = y / elmnt.scrollHeight;
     
+    if (scrollpercent == 1) {
+        document.getElementById("nice").setAttribute("style", "opacity: ''")
+    };
+    
     var draw = length_reitti * scrollpercent;
 
     reitti.style.strokeDashoffset = length_reitti - draw;
 }
-
 
 // VIDEO AUTOPLAY
 
@@ -135,21 +136,50 @@ function checkSroll() {
 window.addEventListener("scroll", checkSroll, false);
 window.addEventListener("resize", checkSroll, false);
 
+var isVisible = false;
+
+function showTimer() {
+    var elmnt = document.getElementById("timerbox");
+    var timer = document.getElementById("showTimer");
+    if (!isVisible) {
+        elmnt.setAttribute("style", "display: block;");
+        timer.innerHTML = "Hide";
+        isVisible = true;
+    } else {
+        elmnt.setAttribute("style", "");
+        timer.innerHTML = "Show";
+        isVisible = false;
+    }
+}
 
 // BG
 
-var trigger_bg = new Waypoint({
-    element: document.getElementById("bg2"),
+/*var bg_section = new Waypoint({
+    element: document.getElementById("bg-trigger"),
     handler: function(direction) {
         if (direction == "down") {
-            document.getElementById("bg-section").setAttribute("style", "background-image: url(./media/images/bg2.jpeg)")
+            document.getElementById("changing-img").setAttribute("style", "position: fixed; top: 0;");
         }
         
         if (direction == "up") {
-            document.getElementById("bg-section").removeAttribute("style", "background-image: url(./media/images/bg2.jpeg)")
+            document.getElementById("changing-img").setAttribute("style", "position: absolute;");
+        }
+    }
+})*/
+
+var trigger_bg = new Waypoint({
+    element: document.getElementById("bg-trigger"),
+    handler: function(direction) {
+        if (direction == "down") {
+            document.getElementById("bg1").setAttribute("style", "opacity: 0;");
+            document.getElementById("bg2").setAttribute("style", "opacity: 1;");
+        }
+        
+        if (direction == "up") {
+            document.getElementById("bg1").setAttribute("style", "opacity: 1;");
+            document.getElementById("bg2").setAttribute("style", "opacity: 0;");
         }
     },
     offset: "50%"
 });
-
 
